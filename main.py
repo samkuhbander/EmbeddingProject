612b0b01dee2f010c3e0e5360ccbbc2bb919b8bd
samkuhbander <55243087+samkuhbander@users.noreply.github.com>
2023-08-19 15:47:57 -0400

    set up basic embedding storage in milvus

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import glob
 import numpy as np
 from parsers import parse_methods
 from embedding import embed_sentences
-import csv
+import numpy as np
+from pymilvus import (
+    connections,
+    utility,
+    FieldSchema, CollectionSchema, DataType,
+    Collection,
+)
+import time
 
 directory_path = 'ExampleProject/*'
 files = glob.glob(directory_path)
@@ -20,8 +27,31 @@ for file_path in files:
         print(method_or_chunk)
         print("-" * 50)
 
-# Saving as a .csv file
-with open('embeddings.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerows(all_embeddings)
-    
+fmt = "\n=== {:30} ===\n"
+# Connect to Milvus
+print(fmt.format("start connecting to Milvus"))
+connections.connect("default", host="localhost", port="19530")
+
+# Check if the collection exists and create if not
+has = utility.has_collection("hello_milvus")
+print(f"Does collection hello_milvus exist in Milvus: {has}")
+
+dim = 768  # Embedding dimension
+num_entities = 512  # Sequence Length
+
+fields = [
+    FieldSchema(name="pk", dtype=DataType.VARCHAR, is_primary=True, auto_id=False, max_length=100),
+    FieldSchema(name="random", dtype=DataType.DOUBLE),
+    FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=dim)
+]
+
+schema = CollectionSchema(fields, "hello_milvus is the simplest demo to introduce the APIs")
+hello_milvus = Collection("hello_milvus", schema, consistency_level="Strong")
+
+# Start inserting entities
+print(fmt.format("Start inserting entities"))
+entities = [
+    [str(i) for i in range(num_entities)],
+    np.random.random(num_entities).tolist(), # If you want to keep the random field
+    all_embeddings
+]
