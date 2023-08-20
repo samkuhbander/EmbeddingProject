dd8c0e8885fb3119c055f0155e57114f4f3c85ac
samkuhbander <55243087+samkuhbander@users.noreply.github.com>
2023-08-19 22:25:26 -0400

    Added file hash in each database entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor
 import glob
+import hashlib
 from parsers import parse_file
 from embedding import embed_sentences
 from milvus_DB import connect_to_milvus, create_collection, create_index, does_db_exist, insert_entities
@@ -8,7 +9,15 @@ def process_file(file_path):
     print(f"Parsing file: {file_path}")
     methods_or_chunks = parse_file(file_path)
     embeddings = embed_sentences(methods_or_chunks)
-    return embeddings, methods_or_chunks
+    file_hash = hash_file(file_path)
+    return embeddings, methods_or_chunks, file_hash
+
+def hash_file(file_path):
+    with open(file_path, 'rb') as f:
+        file_hash = hashlib.sha256()
+        while chunk := f.read(8192):
+            file_hash.update(chunk)
+    return int(file_hash.hexdigest(), 16) % 10**16
 
 dim = 768
 
@@ -28,17 +37,19 @@ else:
 # Collecting embeddings for all the chunks
 all_embeddings = []
 all_code_snippets = []
+all_file_hashes = []
 
 with ThreadPoolExecutor() as executor:
     results = list(executor.map(process_file, files))
 
-for embeddings, methods_or_chunks in results:
+for embeddings, methods_or_chunks, file_hash in results:
     all_embeddings.extend(embeddings)
     all_code_snippets.extend(methods_or_chunks)
+    all_file_hashes.extend([file_hash] * len(methods_or_chunks))
 
 num_entities = len(all_embeddings)
 
-insert_entities(milvus_DB, num_entities, all_embeddings, all_code_snippets)
+insert_entities(milvus_DB, num_entities, all_embeddings, all_code_snippets, all_file_hashes)
 create_index(milvus_DB)
 print("Done adding entities to Milvus_DB")
 
--- milvus_DB.py
+++ milvus_DB.py
@@ -1,3 +1,4 @@
+import hashlib
 import numpy as np
 from pymilvus import (
     connections,
@@ -5,7 +6,6 @@ from pymilvus import (
     FieldSchema, CollectionSchema, DataType,
     Collection
 )
-from hashlib import sha256
 
 def connect_to_milvus():
     print("\n=== start connecting to Milvus ===\n")
@@ -16,31 +16,31 @@ def does_db_exist():
     return utility.has_collection("milvus_DB")
 
 def create_collection(dim):
-    #if database exists, drop it
+    # if database exists, drop it
     if does_db_exist():
         print("\n=== Drop collection 'milvus_DB' ===\n")
         utility.drop_collection("milvus_DB")
 
     print("\n=== Create collection 'milvus_DB' ===\n")
     fields = [
-        FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False), # set auto_id to False
+        FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=True), # set auto_id to True
         FieldSchema(name="random", dtype=DataType.DOUBLE),
         FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=dim),
-        FieldSchema(name="code_snippet", dtype=DataType.VARCHAR, max_length=10000)
+        FieldSchema(name="code_snippet", dtype=DataType.VARCHAR, max_length=10000),
+        FieldSchema(name="file_hash", dtype=DataType.INT64) # Field for file hash
     ]
 
     schema = CollectionSchema(fields, description="introducing melvis 'milvus_DB'")
     milvus_DB = Collection("milvus_DB", schema, consistency_level="Strong")
     return milvus_DB
 
-def insert_entities(milvus_DB, num_entities, all_embeddings, all_code_snippets):
+def insert_entities(milvus_DB, num_entities, all_embeddings, all_code_snippets, all_file_hashes):
     print("\n=== Start inserting entities ===\n")
-    pks = [int(sha256(snippet.encode()).hexdigest(), 16) % 10**16 for snippet in all_code_snippets] # Creating a hash as pk
     entities = [
-        pks,
         np.random.random(num_entities).tolist(),
         all_embeddings,
-        all_code_snippets
+        all_code_snippets,
+        all_file_hashes
     ]
 
     insert_result = milvus_DB.insert(entities)
