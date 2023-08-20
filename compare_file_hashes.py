5e8263e7db2b14b35fde80b19ab579ec9170c9d5
samkuhbander <55243087+samkuhbander@users.noreply.github.com>
2023-08-20 14:49:20 -0400

    Fixed formatting

--- compare_file_hashes.py
+++ compare_file_hashes.py
@@ -1,33 +1,38 @@
 import glob
 import hashlib
+from pymilvus import Collection
 from milvus_DB import drop_altered_file
-from pymilvus import (
-    Collection
-)
+
 
 def hash_file(file_path):
-    with open(file_path, 'rb') as f:
+    with open(file_path, "rb") as f:
         file_hash = hashlib.sha256()
         while chunk := f.read(8192):
             file_hash.update(chunk)
     return int(file_hash.hexdigest(), 16) % 10**16
 
+
 def compareFiles():
-    collection = Collection("milvus_DB")      # Get an existing collection.
+    collection = Collection("milvus_DB")  # Get an existing collection.
     collection.load()
 
-    directory_path = 'ExampleProject/*'
+    directory_path = "ExampleProject/*"
     original_file_paths = glob.glob(directory_path)
     filtered_file_paths = []  # We'll store the unmatched files here
-    
+
     for file_path in original_file_paths:
         hashed_file = hash_file(file_path)
-        if len(collection.query(
-            expr = str(hashed_file) + " == file_hash",
-            offset = 0,
-            output_fields = ["file_hash"],
-            limit = 1,
-            )) != 1:
+        if (
+            len(
+                collection.query(
+                    expr=str(hashed_file) + " == file_hash",
+                    offset=0,
+                    output_fields=["file_hash"],
+                    limit=1,
+                )
+            )
+            != 1
+        ):
             # If not found in the collection, add to the filtered list
             filtered_file_paths.append(file_path)
         else:
--- embedding.py
+++ embedding.py
@@ -1,7 +1,8 @@
 # embedding.py
 from FlagEmbedding import FlagModel
 
+
 def embed_sentences(code_chunks):
-    model = FlagModel('BAAI/bge-base-en')
+    model = FlagModel("BAAI/bge-base-en")
     embeddings = model.encode(code_chunks)
     return embeddings
